e91fe55478b58d61eb1673266d4e3a807441d2a6
Khangai <khangai8888@gmail.com>
2023-11-09 23:49:19 +0900

    Fixed chessdotcom API not importing and get_player_rating

--- leaderboard.py
+++ leaderboard.py
@@ -1,9 +1,14 @@
 import pprint
-from chessdotcom import get_leaderboards, get_player_stats
+from chessdotcom import get_leaderboards, get_player_stats, Client
 
-# pprint shows data, especially API data in a readable format
-#printer = pprint.PrettyPrinter()
-#printer.pprint(get_leaderboards().json)
+Client.request_config["headers"]["User-Agent"] = (
+  "My Python Application"
+)
+
+#pprint shows data, especially API data in a readable format
+def pretty_print(bla):
+  printer = pprint.PrettyPrinter()
+  printer.pprint(bla)
 
 def print_category_leaderboard():
   data = get_leaderboards().json
@@ -13,6 +18,7 @@ def print_category_leaderboard():
   print('Which category leaderboard would you like to see?')
   for category in categories:
     print(category)
+
   selected_category = input()
 
   if selected_category in categories:
@@ -25,15 +31,15 @@ def get_player_rating():
   print('Which player\'s stats would you like to see?')
   selected_player = input()
 
-  try:
-    data = get_player_stats(selected_player).json
-  except:
-    print('Player not found.')
-    return
-  else:
-    categories = ['chess_blitz', 'chess_rapid', 'chess_bullet']
-    for category in categories:
-      print(f"Category: {category} | Current Rating: {data[category]['last']['rating']}")
+  data = get_player_stats(selected_player).json['stats']
 
-print_category_leaderboard()
+  #using for debugging
+  #pretty_print(data)
+
+  categories = ['chess_blitz', 'chess_rapid', 'chess_bullet']
+
+  for category in categories:
+    print(f"Category: {category} | Current Rating: {data[category]['last']['rating']}")
+
+#print_category_leaderboard()
 get_player_rating()
